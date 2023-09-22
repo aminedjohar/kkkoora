cbea884ee11eefff93465786026bec335a5d1308
aminedjohar <aminedjohar@gmail.com>
2023-09-22 11:27:24 +0100

    Created Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between items-center py-2">
+            <Link href="/" className="flex items-center space-x-2">
+                <div className="relative w-[30px] h-[30px]">
+                    <Image
+                        fill
+                        src="/footinfo.png"
+                        alt="logo"
+                        className="object-cover"
+                    />
+                </div>
+                <span className="text-2xl font-bold none md:block">kkkoora </span>
+            </Link>
+            
+        </div>
+    );
+}
+
+export default Navbar;
